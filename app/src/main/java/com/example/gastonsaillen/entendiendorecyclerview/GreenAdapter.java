dfa7e2629a66dd91f17e2be5f21fc0caef485c63
GastonSaillen <gsaillen@ottaaproject.com>
2018-06-08 12:00:41 -0300

    Entendiendo RecyclerView #1
    - Crear RecyclerView
    - Añadir Layout a la vista
    - Bindear datos a cada vista
    - Clicar cada elemento del RecyclerView

---
+++ app/src/main/java/com/example/gastonsaillen/entendiendorecyclerview/GreenAdapter.java
@@ -0,0 +1,93 @@
+package com.example.gastonsaillen.entendiendorecyclerview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
+
+
+    /** Declaramos la cantidad de vistas que vamos a tener */
+    private int mNumberItems;
+
+    /** Creamos el Listener que va a escuchar por la posicion a clicar en la lista */
+    final private ListItemClickListener mOnClickListener;
+
+    /**Vamos a utilizar este constructor para pasar un entero de la cantidad de vistas que queremos y asignarselo a mNumberItems*/
+    public GreenAdapter(int numberOfItems,ListItemClickListener listener){
+    //Poblamos la cantidad de vistas que queremos mostrar
+        mNumberItems = numberOfItems;
+     //Le decimos al adaptador que item clicamos
+    mOnClickListener = listener;
+    }
+
+    /**Creamos una interfaz que va a definir el listener*/
+    public interface ListItemClickListener{
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    @NonNull
+    @Override
+    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Context mContext = parent.getContext(); //Pasamos el contexto de donde se va a inflar la vista
+        int layoutIdForListItem = R.layout.number_list_item; //buscamos el layout de la vista para el viewHolder
+        LayoutInflater inflater = LayoutInflater.from(mContext); // Creamos un layoutInflater que sirve para bindear esa vista a cada elemento del Recycler
+        boolean shouldAttachToParentImmediately = false; //Sirve para no poblar rapidamente la vista al padre
+
+        View view = inflater.inflate(layoutIdForListItem,parent,shouldAttachToParentImmediately); //Creamos una vista donde inflamos la layout ya antes llamada , le pasamos el parent donde se va a mostrar y atachamos el boolean
+        NumberViewHolder viewHolder = new NumberViewHolder(view); //Llamamos a la clase NumberViewHolder y le pasamos la vista para cada elemento
+
+        return viewHolder; //retornamos un View
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
+        holder.bind(position); //Usamos el NumberViewHolder para poder bindear datos adentro de cada vista
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems; //Devolvemos la cantidad de items que va a tener el recycler a especificar
+    }
+
+
+    /**Creamos el viewHolder para llamar al texto que creamos en el number_list_item
+     * Implementamos la interfaz onClickListener para que se invoque en cada vista que clicamos*/
+
+    class NumberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView listItemNumberView;
+
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+
+            //Encontramos el textview que creamos en el layout
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+            //Le decimos que se utilize clickListener en este constructor
+            itemView.setOnClickListener(this);
+
+        }
+
+        /**Nos va a servir para bindear datos a las vistas creadas con el viewHolder*/
+        void bind(int listIndex){
+
+            //Le seteamos al textview buscado en el viewHolder el valor de listIndex que es un entero
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
+
+        @Override
+        public void onClick(View view) {
+            //Obtenemos la posicion en el adaptador que clicamos
+            int clickedPosition = getAdapterPosition();
+            //Al final le pasamos al click listener la posicion que acabamos de clicar
+            mOnClickListener.onListItemClick(clickedPosition);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gastonsaillen/entendiendorecyclerview/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.gastonsaillen.entendiendorecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements GreenAdapter.ListItemClickListener {
+
+    //Definimos la cantidad de items en la lista
+    private static final int NUM_LIST_ITEMS = 100;
+    // Llamamos a GreenAdapter para poder utilizarlo en nuestro RecyclerView
+    private GreenAdapter mAdapter;
+    //Creamos el recyclerView
+    private RecyclerView mNumberList;
+    //Creamos un Toast global para evitar que se muestren repetidamente cuando el usuario clica muy rapidamente en los elementos
+    private Toast mToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNumberList = (RecyclerView) findViewById(R.id.rv_numbers);
+        //Creamos un divider por cada elemento del RV de manera vertical
+        mNumberList.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        //Creamos un LinearLayout con el manager para mostrar el RecyclerView de forma de lista
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //Le seteamos al recyclerView el layoutManager, en este caso LinearLayout
+        mNumberList.setLayoutManager(linearLayoutManager);
+       //En true hacemos optimizacion de la UI en nuestro RV, evita que se invalide la layout cuando el contenido del adaptador cambia
+        mNumberList.setHasFixedSize(true);
+        //Le pasamos al adaptador la cantidad de items
+        //Le pasamos this por que el listener necesita un contexto de donde tomar ese click, en este caso es esta activity
+        mAdapter = new GreenAdapter(NUM_LIST_ITEMS,this);
+        //Finalmente le seteamos al RV el adaptador
+        mNumberList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
+        //Con esto chequeamos que no se vayan encolando los textView y al clicar en distintos items solo nos muestre el que clicamos
+        if(mToast!= null){
+            mToast.cancel();
+        }
+        String toastMessage = "Item #" + clickedItemIndex + " clicked";
+        mToast = Toast.makeText(this,toastMessage,Toast.LENGTH_LONG);
+        mToast.show();
+
+    }
+}
